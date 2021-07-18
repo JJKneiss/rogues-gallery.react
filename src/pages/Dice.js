cea4b2ae893d770677a15265f5c8b0170503dda8
JJKneiss <54962857+JJKneiss@users.noreply.github.com>
2021-07-18 02:07:00 -0400

    test

---
+++ src/pages/Dice.js
@@ -0,0 +1,9 @@
+import Header from "../components/Header./Header"
+function Dice() {
+    return (
+        <div className="App">
+            <Header />
+        </div>
+    )
+}
+export default Dice;
